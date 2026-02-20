3971005a45a752be2cc1bbbdbd7ad0b55f5ecd37
Erick Raposo <eantunesraposo@gmail.com>
2026-02-20 00:43:54 -0300

    [FIX] - adiciona PATCH ao cors do backend

--- backend/src/main/java/com/grupo21/kcalma/infra/cors/CorsConfig.java
+++ backend/src/main/java/com/grupo21/kcalma/infra/cors/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:5173"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
         config.setAllowCredentials(true);
