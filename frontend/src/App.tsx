9e11f79c08451c3d696bb679150c9f1c197a864d
Erick Raposo <eantunesraposo@gmail.com>
2026-02-23 20:34:54 -0300

    [FIX] - adiciona tratamento de erros no backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import { Toaster } from 'sonner'
-import { AuthProvider } from './context/AuthContext'
+import { AuthProvider, Context } from './context/AuthContext'
 import { AppRouter } from './routes/AppRouter'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useContext, useEffect } from 'react'
+import { setupInterceptors } from './services/authInterceptor'
 
 const queryClient = new QueryClient()
 
 function App() {
+  const context = useContext(Context)
+
+  useEffect(() => {
+    if (context?.handleLogout) {
+      setupInterceptors(context.handleLogout)
+    }
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
