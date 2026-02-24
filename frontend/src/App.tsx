627baaee7652f5eea2dfc9f34a3f329e9d8d021f
Erick Raposo <eantunesraposo@gmail.com>
2026-02-24 08:55:14 -0300

    [UPD] - implementa refresh token no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
 import { Toaster } from 'sonner'
-import { AuthProvider } from './context/AuthContext'
+import { Context } from './context/AuthContext'
 import { AppRouter } from './routes/AppRouter'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+import { useContext, useEffect } from 'react'
+import { setupInterceptors } from './services/authInterceptor'
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <AppRouter />
-        <Toaster position='top-center' />
-      </AuthProvider>
-    </QueryClientProvider>
-  )
+  const context = useContext(Context)
+
+  useEffect(() => {
+    if (context?.handleLogout) {
+      setupInterceptors(context.handleLogout)
+    }
+  }, [])
+
+  return (<>
+    <AppRouter />
+    <Toaster position='top-center' />
+  </>)
 }
 
 export default App
---
+++ frontend/src/services/authInterceptor.ts
@@ -0,0 +1,56 @@
+import axios from "axios"
+import { api, BASE_URL } from "./api"
+
+export function setupInterceptors(handleLogout: () => void) {
+    api.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+            const originalRequest = error.config
+
+            if (
+                error.response?.status === 401 &&
+                !originalRequest._retry
+            ) {
+                originalRequest._retry = true
+
+                const refreshToken = localStorage.getItem("@Auth:refreshToken")
+
+                if (refreshToken) {
+                    try {
+                        const response = await axios.post(
+                            `${BASE_URL}/auth/refresh-token`,
+                            {
+                                refresh_token: JSON.parse(refreshToken),
+                            }
+                        )
+
+                        const newAccessToken = response.data.access_token
+
+                        localStorage.setItem(
+                            "@Auth:token",
+                            JSON.stringify(newAccessToken)
+                        )
+
+                        originalRequest.headers[
+                            "Authorization"
+                        ] = `Bearer ${newAccessToken}`
+
+                        return api(originalRequest)
+                    } catch {
+                        handleLogout()
+                    }
+                } else {
+                    handleLogout()
+                }
+            }
+
+            if (
+                error.response?.status === 401
+            ) {
+                handleLogout()
+            }
+
+            return Promise.reject(error)
+        }
+    )
+}
