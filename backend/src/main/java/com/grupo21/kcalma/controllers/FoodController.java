5a78b45ba81657933e143825fe753a0d29404ac3
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-02-25 15:20:15 -0300

    [ADD] - Adiciona endpoints de busca do alimento

--- backend/src/main/java/com/grupo21/kcalma/controllers/FoodController.java
+++ backend/src/main/java/com/grupo21/kcalma/controllers/FoodController.java
@@ -1,16 +1,15 @@
 package com.grupo21.kcalma.controllers;
 
-import com.grupo21.kcalma.domain.food.Food;
 import com.grupo21.kcalma.dto.AddFoodDTO;
+import com.grupo21.kcalma.dto.FoodByNameRequestDTO;
 import com.grupo21.kcalma.dto.FoodResponseDTO;
 import com.grupo21.kcalma.services.FoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/food")
@@ -21,6 +20,20 @@ public class FoodController {
     @PostMapping("/add")
     public ResponseEntity<FoodResponseDTO> addFood(@RequestBody AddFoodDTO food, Principal connectedUser){
         FoodResponseDTO response = foodService.addFood(food, connectedUser);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<FoodResponseDTO>> getAll(Principal connectedUser){
+        List<FoodResponseDTO> response = foodService.getAll(connectedUser);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<FoodResponseDTO> getByName(@RequestBody FoodByNameRequestDTO data, Principal connectedUser){
+        FoodResponseDTO response = foodService.getByName(data, connectedUser);
 
         return ResponseEntity.ok(response);
     }
--- backend/src/main/java/com/grupo21/kcalma/services/FoodService.java
+++ backend/src/main/java/com/grupo21/kcalma/services/FoodService.java
@@ -3,14 +3,15 @@ package com.grupo21.kcalma.services;
 import com.grupo21.kcalma.domain.food.Food;
 import com.grupo21.kcalma.domain.user.User;
 import com.grupo21.kcalma.dto.AddFoodDTO;
+import com.grupo21.kcalma.dto.FoodByNameRequestDTO;
 import com.grupo21.kcalma.dto.FoodResponseDTO;
 import com.grupo21.kcalma.repositories.FoodRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +29,21 @@ public class FoodService {
         food.setCalories(dados.calories());
 
         return FoodResponseDTO.create(foodRepository.save(food));
+    }
+
+    public List<FoodResponseDTO> getAll(Principal connectedUser) {
+        User user = userService.getAuthenticatedUser(connectedUser);
+
+        List<Food> foods = foodRepository.getAllByUser(user);
+
+        return foods.stream().map(FoodResponseDTO::create).collect(Collectors.toList());
+    }
+
+    public FoodResponseDTO getByName(FoodByNameRequestDTO data, Principal connectedUser) {
+        User user = userService.getAuthenticatedUser(connectedUser);
+
+        Food food = foodRepository.getByNameAndUser(data.name(), user);
+
+        return FoodResponseDTO.create(food);
     }
 }
