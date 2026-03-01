f72069d6d4f9f3ec49fbbe571c0e570e631ab8b2
Pedro Furlan <pedrogfurlan1@gmail.com>
2026-02-28 23:27:29 -0300

    [UPD] - Adiciona exceptions em Food e Meal services

--- backend/src/main/java/com/grupo21/kcalma/services/FoodService.java
+++ backend/src/main/java/com/grupo21/kcalma/services/FoodService.java
@@ -3,6 +3,8 @@ package com.grupo21.kcalma.services;
 import com.grupo21.kcalma.domain.food.Food;
 import com.grupo21.kcalma.domain.user.User;
 import com.grupo21.kcalma.dto.*;
+import com.grupo21.kcalma.exceptions.NotFoundException;
+import com.grupo21.kcalma.exceptions.UserNotAllowedException;
 import com.grupo21.kcalma.repositories.FoodRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,10 @@ public class FoodService {
     private final FoodRepository foodRepository;
 
     public FoodResponseDTO addFood(AddFoodRequestDTO dados, Principal connectedUser) {
+
+        if (dados.calories() < 0)
+            throw new IllegalArgumentException("As calorias devem ser um valor maior ou igual a zero.");
+
         User user = userService.getAuthenticatedUser(connectedUser);
         Food food = new Food();
 
@@ -44,6 +50,9 @@ public class FoodService {
 
         Food food = foodRepository.getByNameAndUser(data.name(), user);
 
+        if(food==null)
+            throw new NotFoundException("Nenhum alimento encontrado com esse nome e usuário");
+
         return FoodResponseDTO.create(food);
     }
 
@@ -52,32 +61,37 @@ public class FoodService {
 
         Optional<Food> opFood= foodRepository.findById(data.id());
 
-        if(opFood.isPresent()){
-            Food food = opFood.get();
+        if (opFood.isEmpty())
+            throw new NotFoundException("Nenhum alimento encontrado com esse Id.");
 
-            if(food.getUser().equals(user)){
-                foodRepository.deleteById(data.id());
-            }
-        }
+        Food food = opFood.get();
+
+        if(!food.getUser().equals(user))
+            throw new UserNotAllowedException("O alimento não pertence ao usuário.");
+
+        foodRepository.deleteById(data.id());
     }
     
     public FoodResponseDTO updateFood(UpdateFoodRequestDTO data, Principal connectedUser){
         User user = userService.getAuthenticatedUser(connectedUser);
         Optional<Food> opFood = foodRepository.findById(data.getId());
 
-        if(opFood.isPresent()){
-            Food food = opFood.get();
+        if(opFood.isEmpty())
+            throw new NotFoundException("Nenhum alimento encontrado com esse Id.");
 
-            if(food.getUser().equals(user)){
-                if(data.getName()!=null) food.setName(data.getName());
-                if(data.getMeasureUnit()!=null) food.setMeasureUnit(data.getMeasureUnit());
-                if(data.isUpdateCalories()) food.setCalories(data.getCalories());
+        Food food = opFood.get();
 
-                Food updatedFood = foodRepository.save(food);
+        if(!food.getUser().equals(user))
+            throw new UserNotAllowedException("O alimento não pertence ao usuário.");
 
-                return FoodResponseDTO.create(updatedFood);
-            }
+        if(data.getName()!=null) food.setName(data.getName());
+        if(data.getMeasureUnit()!=null) food.setMeasureUnit(data.getMeasureUnit());
+        if(data.isUpdateCalories()) {
+            if (data.getCalories() < 0)
+                throw new IllegalArgumentException("As calorias devem ser um valor maior ou igual a zero.");
+
+            food.setCalories(data.getCalories());
         }
-        return null; //temporário
+        return FoodResponseDTO.create(foodRepository.save(food));
     }
 }
